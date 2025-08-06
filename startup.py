90be1194749feef6d5db2825d5b10441b19449c7
Rahul Katiyar <rahul.katiyar@auraml.com>
2025-08-06 08:42:44 +0000

    Add .gitignore to exclude __pycache__ and update USD_PATH and TIMEOUT in startup.py

--- startup.py
+++ startup.py
@@ -3,8 +3,8 @@ import threading
 from simulation_manager import run_isaac_sim, shutdown, keep_alive, set_keep_running
 
 # Configuration
-USD_PATH = "/uploads/Collected_lift_demo/new_lift.usd"  # Change this to your USD file path
-TIMEOUT = 200  # Timeout for stage loading in seconds
+USD_PATH = "https://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/Isaac/4.5/Isaac/Environments/Simple_Warehouse/warehouse.usd"  # Change this to your USD file path
+TIMEOUT = 60  # Timeout for stage loading in seconds
 
 def timeout_monitor():
     """Background thread to monitor timeout"""
